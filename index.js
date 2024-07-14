85f4e7bf0f9337c8d816f030f357535329576cff
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2024-07-14 19:05:01 +0530

    fix

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
-const {execSync} =require("child_process")
+const { execSync } = require("child_process");
 
-const runCommand=command=>{
-    try{
-execSync(`${command}`,{stdio:"inherit"})
-    }catch(e){
-        console.error(`Failed to execute ${command}` ,e)
-    }
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+  }
+};
+
+const repoName = process.argv[2];
+
+if (!repoName || repoName === "") {
+  console.error("Must be add project folder name");
+  process.exit(-1);
 }
-
-const repoName= process.argv[2];
-
-if(!repoName || repoName==="" ){
-    console.error("Must be add project folder name")
-    process.exit(-1);
-}
-const gitCheckoutCommand=`git clone --depth 1 https://github.com/Ajnash-ibn-umer/vegamjs-starter ${repoName}`;
-const installCommand=`cd ${repoName} && npm i`
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Ajnash-ibn-umer/vegamjs-starter ${repoName}`;
+const installCommand = `cd ${repoName} && npm i`;
 
 console.log(`Cloning repository ${repoName}`);
-const checkout=runCommand(gitCheckoutCommand);
-if(!checkout) process.exit(-1);
+const checkout = runCommand(gitCheckoutCommand);
+if (!checkout) process.exit(-1);
 
 console.log(`Installing dependencies for ${repoName}`);
-const installDep=runCommand(installCommand);
-if(!installDep) process.exit(-1);
+const installDep = runCommand(installCommand);
+if (!installDep) process.exit(-1);
 
 console.log("Congratulation you are ready");
-
